e320c9ad893bada241750d07c74d307d8e35ead9
swati koli <119852517+siya217@users.noreply.github.com>
2026-02-14 13:13:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
 import streamlit as st
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 
-st.set_page_config(page_title="Test App")
+st.title("Customer Segmentation using Clustering")
 
-st.title("Hello Siya 👋")
-st.write("Agar ye page dikh raha hai, to Streamlit 100% kaam kar raha hai.")
-st.success("Deployment successful 🎉")
+st.write("App started...")
+
+# Load data
+df = pd.read_excel("marketing_campaign1.xlsx")
+
+st.write("Data loaded successfully")
+
+# Cleaning
+df['Income'].fillna(df['Income'].median(), inplace=True)
+df['Age'] = 2025 - df['Year_Birth']
+
+features = [
+    'Age','Income','Recency',
+    'MntWines','MntFruits','MntMeatProducts',
+    'MntFishProducts','MntSweetProducts','MntGoldProds',
+    'NumWebPurchases','NumStorePurchases','NumCatalogPurchases'
+]
+
+X = df[features]
+
+# Scaling
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Clustering
+kmeans = KMeans(n_clusters=4, random_state=42)
+df['Cluster'] = kmeans.fit_predict(X_scaled)
+
+st.subheader("Clustered Customers")
+st.dataframe(df[['ID','Age','Income','Cluster']])
+
+st.subheader("Cluster Summary")
+st.dataframe(df.groupby('Cluster')[features].mean())
+
+
