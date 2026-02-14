a7d920224c204e29dfdee45c6999230b3b5cb279
swati koli <119852517+siya217@users.noreply.github.com>
2026-02-14 13:09:32 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Test App")
+
+st.title("Hello Siya 👋")
+st.write("Agar ye page dikh raha hai, to Streamlit 100% kaam kar raha hai.")
+st.success("Deployment successful 🎉")
